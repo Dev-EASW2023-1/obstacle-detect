bc7a05c214b3913adecf6050a1c40b91d1e70a73
mk3058 <kmk3058@icloud.com>
2023-05-23 20:42:12 +0900

    feat(image-upload): add image upload feature

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+require('dotenv').config();
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const express = require('express');
+const multer = require('multer');
+const AWS = require('aws-sdk');
+const Jimp = require('jimp');
+const fs = require('fs');
 
-export default App;
+const app = express();
+
+AWS.config.update({
+	region: process.env.AWS_REGION,
+	accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+	secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+});
+
+const s3 = new AWS.S3();
+
+const upload = multer({ dest: 'uploads/' });
+
+app.post('/upload', upload.single('image'), async (req, res) => {
+	const fileContent = fs.readFileSync(req.file.path);
+	const params = {
+	  Bucket: process.env.AWS_S3_BUCKET_NAME,
+	  Key: req.file.originalname,
+	  Body: fileContent
+	};
+	const data = await s3.upload(params).promise();
+	res.json({ imageName: req.file.originalname });
+  });
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.listen(3000, () => console.log('Server running on port 3000'));
