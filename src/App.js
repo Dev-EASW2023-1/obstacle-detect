258602425d93ba49fc870a164b9f5861ba3b3d32
mk3058 <kmk3058@icloud.com>
2023-05-30 14:39:27 +0900

    refactor(optimize): add exception handling and optimize bounding box calculations

--- src/App.js
+++ src/App.js
@@ -20,86 +20,99 @@ const rekognition = new AWS.Rekognition();
 const upload = multer({ dest: 'uploads/' });
 
 app.post('/upload', upload.single('image'), async (req, res) => {
-	const fileContent = fs.readFileSync(req.file.path);
-	const params = {
-	  Bucket: process.env.AWS_S3_BUCKET_NAME,
-	  Key: req.file.originalname,
-	  Body: fileContent
-	};
-	const data = await s3.upload(params).promise();
-	res.json({ imageName: req.file.originalname });
-});
+	try {
+	  const fileContent = fs.readFileSync(req.file.path);
+	  const params = {
+		Bucket: process.env.AWS_S3_BUCKET_NAME,
+		Key: req.file.originalname,
+		Body: fileContent
+	  };
+	  const data = await s3.upload(params).promise();
+	  res.json({ imageName: req.file.originalname });
+	} catch (error) {
+	  console.error(`Error in /upload: ${error}`);
+	  res.status(500).send('Error uploading image');
+	}
+  });
+
+  const drawBoundingBox = (image, x1, y1, boxWidth, boxHeight, borderThickness) => {
+
+	image.scan(x1, y1, boxWidth, borderThickness, function (x, y, idx) { // Top
+		this.bitmap.data[idx] = 255; // R
+		this.bitmap.data[idx + 1] = 0; // G
+		this.bitmap.data[idx + 2] = 0; // B
+	  });
+	  image.scan(x1, y1, borderThickness, boxHeight, function (x, y, idx) { // Left
+		this.bitmap.data[idx] = 255; // R
+		this.bitmap.data[idx + 1] = 0; // G
+		this.bitmap.data[idx + 2] = 0; // B
+	  });
+	  image.scan(x1, y1 + boxHeight - borderThickness, boxWidth, borderThickness, function (x, y, idx) { // Bottom
+		this.bitmap.data[idx] = 255; // R
+		this.bitmap.data[idx + 1] = 0; // G
+		this.bitmap.data[idx + 2] = 0; // B
+	  });
+	  image.scan(x1 + boxWidth - borderThickness, y1, borderThickness, boxHeight, function (x, y, idx) { // Right
+		this.bitmap.data[idx] = 255; // R
+		this.bitmap.data[idx + 1] = 0; // G
+		this.bitmap.data[idx + 2] = 0; // B
+	  });
+  };
 
 app.get('/analyze/:imageName', async (req, res) => {
-	const params = {
-	  Image: {
-		S3Object: {
-		  Bucket: process.env.AWS_S3_BUCKET_NAME,
-		  Name: req.params.imageName
-		}
-	  },
-	  MaxLabels: 10
-	};
-	const data = await rekognition.detectLabels(params).promise();
-	let image = await Jimp.read(
-	  `https://${process.env.AWS_S3_BUCKET_NAME}.s3.amazonaws.com/${params.Image.S3Object.Name}`
-	);
-	const borderThickness = 3;
-  
-	data.Labels.forEach(label => {
-	  label.Instances.forEach(instance => {
-		const box = instance.BoundingBox;
-		const height = image.bitmap.height;
-		const width = image.bitmap.width;
-  
-		const x1 = Math.floor(box.Left * width);
-		const y1 = Math.floor(box.Top * height);
-		const boxWidth = Math.floor(box.Width * width);
-		const boxHeight = Math.floor(box.Height * height);
+	try {
+		const params = {
+		  Image: {
+			S3Object: {
+			  Bucket: process.env.AWS_S3_BUCKET_NAME,
+			  Name: req.params.imageName
+			}
+		  },
+		  MaxLabels: 10
+		};
+		const data = await rekognition.detectLabels(params).promise();
+		let image = await Jimp.read(
+		  `https://${process.env.AWS_S3_BUCKET_NAME}.s3.amazonaws.com/${params.Image.S3Object.Name}`
+		);
+	  
+		data.Labels.forEach(label => {
+		  label.Instances.forEach(instance => {
+			const box = instance.BoundingBox;
+			const height = image.bitmap.height;
+			const width = image.bitmap.width;
+	  
+			const x1 = Math.floor(box.Left * width);
+			const y1 = Math.floor(box.Top * height);
+			const boxWidth = Math.floor(box.Width * width);
+			const boxHeight = Math.floor(box.Height * height);
+			const centerX = x1 + boxWidth / 2;
+	    	const centerY = y1 + boxHeight / 2;
 
-		const centerX = x1 + boxWidth / 2;
-    	const centerY = y1 + boxHeight / 2;
+			const centerXRange = [width * parseFloat(process.env.CENTER_X_RANGE_START), width * parseFloat(process.env.CENTER_X_RANGE_END)];
+	        const centerYRange = [height * parseFloat(process.env.CENTER_Y_RANGE_START), height * parseFloat(process.env.CENTER_Y_RANGE_END)];
 
-		const centerXRange = [width * 0.25, width * 0.75];
-    	const centerYRange = [height * 0.25, height * 0.75];
+			if (centerX >= centerXRange[0] && centerX <= centerXRange[1] &&
+				centerY >= centerYRange[0] && centerY <= centerYRange[1]) 
+				drawBoundingBox(image, x1, y1, boxWidth, boxHeight, parseInt(process.env.BORDER_THICKNESS));
+		  });
+		});
 
-		if (centerX >= centerXRange[0] && centerX <= centerXRange[1] &&
-			centerY >= centerYRange[0] && centerY <= centerYRange[1]) {
-			image.scan(x1, y1, boxWidth, borderThickness, function (x, y, idx) { // Top
-			  this.bitmap.data[idx] = 255; // R
-			  this.bitmap.data[idx + 1] = 0; // G
-			  this.bitmap.data[idx + 2] = 0; // B
-			});
-			image.scan(x1, y1, borderThickness, boxHeight, function (x, y, idx) { // Left
-			  this.bitmap.data[idx] = 255; // R
-			  this.bitmap.data[idx + 1] = 0; // G
-			  this.bitmap.data[idx + 2] = 0; // B
-			});
-			image.scan(x1, y1 + boxHeight - borderThickness, boxWidth, borderThickness, function (x, y, idx) { // Bottom
-			  this.bitmap.data[idx] = 255; // R
-			  this.bitmap.data[idx + 1] = 0; // G
-			  this.bitmap.data[idx + 2] = 0; // B
-			});
-			image.scan(x1 + boxWidth - borderThickness, y1, borderThickness, boxHeight, function (x, y, idx) { // Right
-			  this.bitmap.data[idx] = 255; // R
-			  this.bitmap.data[idx + 1] = 0; // G
-			  this.bitmap.data[idx + 2] = 0; // B
-			});
-		}
-	  });
-	});
+		const maxDimension = 1000;
+	  if (image.bitmap.width > maxDimension || image.bitmap.height > maxDimension) {
+	    if (image.bitmap.width > image.bitmap.height)
+	      image = image.resize(maxDimension, Jimp.AUTO);
+	    else 
+	      image = image.resize(Jimp.AUTO, maxDimension);
+	  }
 
-	const maxDimension = 1000;
-  if (image.bitmap.width > maxDimension || image.bitmap.height > maxDimension) {
-    if (image.bitmap.width > image.bitmap.height)
-      image = image.resize(maxDimension, Jimp.AUTO);
-    else 
-      image = image.resize(Jimp.AUTO, maxDimension);
-  }
-
-	const buffer = await image.getBufferAsync(Jimp.MIME_PNG);
-	res.set('Content-Type', 'image/png');
-	res.send(buffer);
+		const buffer = await image.getBufferAsync(Jimp.MIME_PNG);
+		res.set('Content-Type', 'image/png');
+		res.send(buffer);
+	}
+	catch (error) {
+		console.error(`Error in /analyze: ${error}`);
+    	res.status(500).send('Error analyzing image');
+	}
 });
 
 app.use(express.static('public'));
